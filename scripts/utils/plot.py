02b56ac97609d55d0986f6cbdca994af0f2d8410
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-11 13:23:45 -0400

    moving to subfolder

--- scripts/utils/plot.py
+++ scripts/utils/plot.py
@@ -2,10 +2,10 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 
-def generate_elbow_plot(dataset):
+def generate_elbow_plot(dataset, cluster_range=31):
     print("Get elbow plot for hexmap clusters ----------------\n\n")
     distortions = []
-    for i in range(1, 31):
+    for i in range(1, cluster_range):
         print(f"starting fit for {i} clusters")
         km = KMeans(
             n_clusters=i, init='random',
@@ -16,26 +16,26 @@ def generate_elbow_plot(dataset):
         distortions.append(km.inertia_)
 
     plt.style.use("fivethirtyeight")
-    plt.plot(range(1, 31), distortions, marker='o')
-    plt.xticks(range(1, 31))
+    plt.plot(range(1, cluster_range), distortions, marker='o')
+    plt.xticks(range(1, cluster_range))
     plt.xlabel('Number of clusters')
     plt.ylabel('Distortion')
     plt.show()
 
-def generate_silhouette_coef_plot(dataset):
+def generate_silhouette_coef_plot(dataset, cluster_range=31):
     print("Get the silhouette coefficients for clusters ----------------\n\n")
     # A list holds the silhouette coefficients for each k
     silhouette_coefficients = []
 
-    for k in range(2, 31):
+    for k in range(2, cluster_range):
         kmeans = KMeans(n_clusters=k)
         kmeans.fit(dataset)
         score = silhouette_score(dataset, kmeans.labels_)
         silhouette_coefficients.append(score)
 
     plt.style.use("fivethirtyeight")
-    plt.plot(range(2, 31), silhouette_coefficients)
-    plt.xticks(range(2, 31))
+    plt.plot(range(2, cluster_range), silhouette_coefficients)
+    plt.xticks(range(2, cluster_range))
     plt.xlabel("Number of Clusters")
     plt.ylabel("Silhouette Coefficient")
     plt.show()
