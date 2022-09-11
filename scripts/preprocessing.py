a370590fefe2efb773c1be160c0c90d25fae169a
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-11 11:52:37 -0400

    small edit

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # read in raw data
-raw_games_df = pd.read_csv('../data/nba.games.stats-raw.csv', header=0)
+raw_games_df = pd.read_csv('../data/nba.games.stats-raw.csv', header=0, index_col=0)
 
 # reduce to only Home games to avoid duplicated inputs
 raw_games_df = raw_games_df[raw_games_df.Home == "Home"]
