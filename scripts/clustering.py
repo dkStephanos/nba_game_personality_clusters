5d0fa8dde29a63c48f5a757714626f16030bdd9a
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-11 21:13:33 -0400

    getting column avgs working

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -2,27 +2,36 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 from utils.plot import generate_elbow_plot, generate_silhouette_coef_plot
-from utils.kmeans import get_distortion_values, show_samples_closest_to_centroid
+from utils.kmeans import get_distortion_values, show_samples_closest_to_centroid, get_column_avgs_per_cluster
 
-N_CLUSTERS = 9
+_N_CLUSTERS = 5
 
 df = pd.read_csv("./data/nba.games.stats-clean.csv", index_col=0)
-df = df.drop(df.columns[list(range(0, 6))], axis=1)
+metadata_df = df[df.columns[list(range(0, 6))]]
+print(metadata_df)
+df.drop(df.columns[list(range(0, 6))], axis=1, inplace=True)
 print(df)
 
-generate_elbow_plot(df, 30)
-generate_silhouette_coef_plot(df, 30)
+# generate_elbow_plot(df, 10)
+# generate_silhouette_coef_plot(df, 10)
 
 print("Running the KMeans clustering model -----------\n\n")
-kmeans = KMeans(n_clusters=N_CLUSTERS, init='random')
+kmeans = KMeans(n_clusters=_N_CLUSTERS, init='random')
 y_km = kmeans.fit_predict(df)
+result_df = metadata_df.join(df)
+result_df["cluster"] = y_km
+print(result_df)
 
-labeled_df = df.copy()
-labeled_df["cluster"] = kmeans.labels_
-print(labeled_df)
+# print("Saving results to csv...")
+#result_df.to_csv("./data/nba.games.stats-clusterResults.csv")
 
-distortion_df = get_distortion_values(kmeans)
-print(distortion_df['cluster'].value_counts())
+print("Getting column averages for each cluster.... ")
+column_avgs_df = get_column_avgs_per_cluster(result_df, _N_CLUSTERS)
+print(column_avgs_df)
+
+# print("Distribution of cluster labels:: ")
+# distortion_df = get_distortion_values(df, kmeans, y_km)
+# print(distortion_df['cluster'].value_counts())
 
 # print("Get the samples closest to the centroids")
-show_samples_closest_to_centroid(df, distortion_df, kmeans, N_CLUSTERS)
+# show_samples_closest_to_centroid(df, distortion_df, kmeans, _N_CLUSTERS)
--- scripts/utils/kmeans.py
+++ scripts/utils/kmeans.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from .constants import NUMERIC_COLS
 
 def get_distortion_values(df, kmeans, y_km):
     distortion = ((df - kmeans.cluster_centers_[y_km]) ** 2.0).sum(axis=1)
@@ -12,3 +13,23 @@ def show_samples_closest_to_centroid(df, distortion_df, kmeans, N_CLUSTERS):
         print(distortion_df.loc[distortion_df['cluster'] == cluster, 'distortion'].sum())
         ind = np.argsort(d)[::][:10]
         print(df.iloc[list(ind)])
+
+def get_column_avgs_per_cluster(df, n_clusters):
+    column_avgs_df = pd.DataFrame(columns=NUMERIC_COLS + ["cluster"])
+    print(column_avgs_df)
+    rows = []
+    for cluster in range(0, n_clusters):
+        temp_dict = {}
+        temp_df = df[df.cluster == cluster]
+        for col in temp_df.columns:
+            if col in NUMERIC_COLS:
+                temp_dict[col] = temp_df[col].mean()
+            elif col == "cluster":
+                temp_dict[col] = temp_df[col].iloc[0]
+        print(temp_dict)
+        rows.append(temp_dict)
+    column_avgs_df = pd.DataFrame.from_dict(rows, orient="columns")
+    print(column_avgs_df)
+    column_avgs_df.to_csv("./data/cluster.stats.column-averages.csv")
+                
+
