26ed73db6669686e216122536baf8870dd448ba6
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-11 21:28:40 -0400

    integrating normalized data

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -6,32 +6,31 @@ from utils.kmeans import get_distortion_values, show_samples_closest_to_centroid
 
 _N_CLUSTERS = 5
 
-df = pd.read_csv("./data/nba.games.stats-clean.csv", index_col=0)
+stats_df = pd.read_csv("./data/nba.games.stats-clean.csv", index_col=0)
+df = pd.read_csv("./data/nba.games.stats-normalized.csv", index_col=0)
 metadata_df = df[df.columns[list(range(0, 6))]]
-print(metadata_df)
 df.drop(df.columns[list(range(0, 6))], axis=1, inplace=True)
-print(df)
 
-# generate_elbow_plot(df, 10)
-# generate_silhouette_coef_plot(df, 10)
+
+generate_elbow_plot(df, 10)
+generate_silhouette_coef_plot(df, 10)
 
 print("Running the KMeans clustering model -----------\n\n")
 kmeans = KMeans(n_clusters=_N_CLUSTERS, init='random')
 y_km = kmeans.fit_predict(df)
-result_df = metadata_df.join(df)
-result_df["cluster"] = y_km
-print(result_df)
+stats_df["cluster"] = y_km
+print(stats_df)
 
-# print("Saving results to csv...")
-#result_df.to_csv("./data/nba.games.stats-clusterResults.csv")
+print("Saving results to csv...")
+stats_df.to_csv("./data/cluster.stats.results-raw.csv")
 
 print("Getting column averages for each cluster.... ")
-column_avgs_df = get_column_avgs_per_cluster(result_df, _N_CLUSTERS)
+column_avgs_df = get_column_avgs_per_cluster(stats_df, _N_CLUSTERS)
 print(column_avgs_df)
 
-# print("Distribution of cluster labels:: ")
-# distortion_df = get_distortion_values(df, kmeans, y_km)
-# print(distortion_df['cluster'].value_counts())
+print("Distribution of cluster labels:: ")
+distortion_df = get_distortion_values(df, kmeans, y_km)
+print(distortion_df['cluster'].value_counts())
 
-# print("Get the samples closest to the centroids")
-# show_samples_closest_to_centroid(df, distortion_df, kmeans, _N_CLUSTERS)
+print("Get the samples closest to the centroids")
+show_samples_closest_to_centroid(df, distortion_df, kmeans, _N_CLUSTERS)
