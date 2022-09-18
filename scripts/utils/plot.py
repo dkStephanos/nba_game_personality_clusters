a6eef91af1bcb8fc2ec039a20d4a7eb29c386458
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-18 18:09:17 -0400

    adding save/show as options params

--- scripts/utils/plot.py
+++ scripts/utils/plot.py
@@ -2,15 +2,19 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 
-def generate_elbow_plot(dataset, cluster_range=31):
+
+def generate_elbow_plot(dataset, cluster_range=31, save=True, show=True):
     print("Get elbow plot for hexmap clusters ----------------\n\n")
     distortions = []
     for i in range(1, cluster_range):
         print(f"starting fit for {i} clusters")
         km = KMeans(
-            n_clusters=i, init='random',
-            n_init=10, max_iter=300,
-            tol=1e-04, random_state=0
+            n_clusters=i,
+            init='random',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0,
         )
         km.fit(dataset)
         distortions.append(km.inertia_)
@@ -20,9 +24,15 @@ def generate_elbow_plot(dataset, cluster_range=31):
     plt.xticks(range(1, cluster_range))
     plt.xlabel('Number of clusters')
     plt.ylabel('Distortion')
-    plt.show()
 
-def generate_silhouette_coef_plot(dataset, cluster_range=31):
+    if save:
+        plt.savefig('./data/elbow_plot.png')
+
+    if show:
+        plt.show()
+
+
+def generate_silhouette_coef_plot(dataset, cluster_range=31, save=True, show=True):
     print("Get the silhouette coefficients for clusters ----------------\n\n")
     # A list holds the silhouette coefficients for each k
     silhouette_coefficients = []
@@ -38,4 +48,9 @@ def generate_silhouette_coef_plot(dataset, cluster_range=31):
     plt.xticks(range(2, cluster_range))
     plt.xlabel("Number of Clusters")
     plt.ylabel("Silhouette Coefficient")
-    plt.show()
+
+    if save:
+        plt.savefig('./data/silhouette_plot.png')
+
+    if show:
+        plt.show()
