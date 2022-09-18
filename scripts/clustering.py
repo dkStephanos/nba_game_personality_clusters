b3c4c529587bf94e3d9167285e78ca22adf1b5e7
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-18 17:53:12 -0400

    splitting up methods

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 from utils.plot import generate_elbow_plot, generate_silhouette_coef_plot
-from utils.kmeans import get_distortion_values, show_samples_closest_to_centroid, get_column_avgs_per_cluster
+from utils.kmeans import (
+    get_distortion_totals_per_cluster,
+    get_samples_closest_to_centroid,
+    get_column_avgs_per_cluster,
+)
 
 _N_CLUSTERS = 5
 
@@ -12,25 +16,27 @@ metadata_df = df[df.columns[list(range(0, 6))]]
 df.drop(df.columns[list(range(0, 6))], axis=1, inplace=True)
 
 
-generate_elbow_plot(df, 10)
-generate_silhouette_coef_plot(df, 10)
+# generate_elbow_plot(df, 10)
+# generate_silhouette_coef_plot(df, 10)
 
 print("Running the KMeans clustering model -----------\n\n")
 kmeans = KMeans(n_clusters=_N_CLUSTERS, init='random')
 y_km = kmeans.fit_predict(df)
 stats_df["cluster"] = y_km
-print(stats_df)
 
-print("Saving results to csv...")
-stats_df.to_csv("./data/cluster.stats.results-raw.csv")
+# print("Saving results to csv...")
+# stats_df.to_csv("./data/cluster.stats.results-raw.csv")
 
 print("Getting column averages for each cluster.... ")
 column_avgs_df = get_column_avgs_per_cluster(stats_df, _N_CLUSTERS)
-print(column_avgs_df)
+# column_avgs_df.to_csv("./data/cluster.stats.results-column-avgs.csv")
 
 print("Distribution of cluster labels:: ")
-distortion_df = get_distortion_values(df, kmeans, y_km)
-print(distortion_df['cluster'].value_counts())
+distortion_df = get_distortion_totals_per_cluster(df, kmeans, y_km, _N_CLUSTERS)
+print(distortion_df)
 
 print("Get the samples closest to the centroids")
-show_samples_closest_to_centroid(df, distortion_df, kmeans, _N_CLUSTERS)
+closest_samples_dict = get_samples_closest_to_centroid(
+    df, stats_df, kmeans, _N_CLUSTERS
+)
+print(closest_samples_dict)
--- scripts/utils/kmeans.py
+++ scripts/utils/kmeans.py
@@ -2,21 +2,32 @@ import pandas as pd
 import numpy as np
 from .constants import NUMERIC_COLS
 
-def get_distortion_values(df, kmeans, y_km):
+
+def _get_distortion_values(df, kmeans, y_km):
     distortion = ((df - kmeans.cluster_centers_[y_km]) ** 2.0).sum(axis=1)
     return pd.DataFrame({'cluster': kmeans.labels_, 'distortion': distortion})
 
-def show_samples_closest_to_centroid(df, distortion_df, kmeans, N_CLUSTERS):
+
+def get_distortion_totals_per_cluster(df, kmeans, y_km, N_CLUSTERS):
+    distortion_df = _get_distortion_values(df, kmeans, y_km)
+    results = {}
     for cluster in range(0, N_CLUSTERS):
-        print(f"\nThe closest samples to cluster {cluster}")
+        results[cluster] = distortion_df.loc[
+            distortion_df['cluster'] == cluster, 'distortion'
+        ].sum()
+    return results
+
+
+def get_samples_closest_to_centroid(df, stats_df, kmeans, N_CLUSTERS, num_samples=4):
+    results = {}
+    for cluster in range(0, N_CLUSTERS):
         d = kmeans.transform(df)[:, cluster]
-        print(distortion_df.loc[distortion_df['cluster'] == cluster, 'distortion'].sum())
-        ind = np.argsort(d)[::][:10]
-        print(df.iloc[list(ind)])
+        results[cluster] = stats_df.iloc[list(np.argsort(d)[::][:num_samples])]
+    return results
+
 
 def get_column_avgs_per_cluster(df, n_clusters):
     column_avgs_df = pd.DataFrame(columns=NUMERIC_COLS + ["cluster"])
-    print(column_avgs_df)
     rows = []
     for cluster in range(0, n_clusters):
         temp_dict = {}
@@ -26,10 +37,6 @@ def get_column_avgs_per_cluster(df, n_clusters):
                 temp_dict[col] = temp_df[col].mean()
             elif col == "cluster":
                 temp_dict[col] = temp_df[col].iloc[0]
-        print(temp_dict)
         rows.append(temp_dict)
     column_avgs_df = pd.DataFrame.from_dict(rows, orient="columns")
-    print(column_avgs_df)
-    column_avgs_df.to_csv("./data/cluster.stats.column-averages.csv")
-                
-
+    return column_avgs_df
