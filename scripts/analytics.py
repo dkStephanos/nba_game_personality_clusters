76e8a013ecb8c3d3179a90891c9d49f86aaa602c
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-09-19 10:05:25 -0400

    WORKING: apriori running
    having major memory issues. might just be that the dataset is too big, but not able to properly run this

--- scripts/analytics.py
+++ scripts/analytics.py
@@ -1,14 +1,16 @@
 import pandas as pd
+from utils.constants import N_CLUSTERS
 from utils.kmeans import (
     get_column_avgs_per_cluster,
     get_cluster_distribution,
 )
 from utils.stats import generate_quantile_truth_table
+from utils.apriori import run_apriori
 
 _save_results = False
 
 print("Reading in data -----------\n\n")
-stats_df = pd.read_csv("./data/cluster.stats.results-raw.csv")
+# stats_df = pd.read_csv("./data/cluster.stats.results-raw.csv", index_col=0)
 
 # print("Getting column averages for each cluster.... ")
 # column_avgs_df = get_column_avgs_per_cluster(stats_df)
@@ -16,8 +18,17 @@ stats_df = pd.read_csv("./data/cluster.stats.results-raw.csv")
 # print("Calculating cluster distribution...")
 # cluster_dist_df = get_cluster_distribution(stats_df)
 
+# print("Generating cluster truth tables...")
+# truth_table_df = generate_quantile_truth_table(stats_df)
+truth_table_df = pd.read_csv(
+    "./data/cluster.stats.results-truth-table.csv", index_col=0
+)
+print(truth_table_df)
+truth_table_df.drop(truth_table_df.columns[list(range(0, 5))], axis=1, inplace=True)
 
-truth_table_df = generate_quantile_truth_table(stats_df)
+for cluster in range(0, N_CLUSTERS):
+    print(f"Running apriori algorithm for cluster {cluster}...")
+    run_apriori(truth_table_df.loc[truth_table_df["cluster"] == cluster], cluster)
 
 if _save_results:
     truth_table_df.to_csv("./data/cluster.stats.results-truth-table.csv")
--- scripts/utils/apriori.py
+++ scripts/utils/apriori.py
@@ -1,25 +1,43 @@
 from apyori import apriori
 
-def run_apriori(dataset, min_support=0.005, min_confidence=0.8, min_length=2):
-    # Collecting the inferred rules in a dataframe 
-    association_rules = list(apriori(dataset.values, min_support=min_support, min_confidence=min_confidence, min_length=min_length))
+
+def run_apriori(dataset, cluster, min_support=0.01, min_confidence=0.9, max_length=4):
+    # Collecting the inferred rules in a dataframe
+    dataset.drop(["cluster"], axis=1, inplace=True)
+    vals = dataset.values
+    temp_vals = []
+    for index, row in enumerate(vals[:10]):
+        temp_vals.append([])
+        for idx, val in enumerate(row):
+            temp_vals[index].append(dataset.columns[idx] + str(val))
+
+    association_rules = apriori(
+        temp_vals,
+        min_support=min_support,
+        min_confidence=min_confidence,
+        max_length=max_length,
+    )
 
     wins = []
     losses = []
-    #Filtering our results to just rules that rhs is survived
-    for result in association_rules:
+    # Filtering our results to just rules that rhs is survived
+    for result in list(association_rules):
         for entry in result.ordered_statistics:
-            if entry.items_add == frozenset({'W'}):
+            print(entry)
+            if entry.items_add == frozenset({'WINorLOSSW'}):
                 wins.append(entry)
-            else:
+            elif entry.items_add == frozenset({'WINorLOSSL'}):
                 losses.append(entry)
 
     print("\nNumber of rules for wins: {0}\n".format(len(wins)))
     print("\nNumber of rules for losses: {0}\n".format(len(losses)))
 
-    #Sorting by lift
-    for unsortedResults in [wins, losses]:
-        sortedResults = sorted(unsortedResults, key=lambda x: x.lift, reverse=True)
-        for result in sortedResults:
-            print(str(result))
-    
+    # Sorting by lift
+    sortedResults = sorted(wins, key=lambda x: x.lift, reverse=True)
+    with open(f'./data/stats.results-apriori-rules-cluster-{cluster}-wins.txt') as f:
+        for result in sortedResults[:1000]:
+            f.write(str(result))
+    sortedResults = sorted(losses, key=lambda x: x.lift, reverse=True)
+    with open(f'./data/stats.results-apriori-rules-cluster-{cluster}-losses.txt') as f:
+        for result in sortedResults[:1000]:
+            f.write(str(result))
