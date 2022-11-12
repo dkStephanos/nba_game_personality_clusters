3ef71bd87998edd7405e7e86ecc69cd462874968
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-11-12 14:09:40 -0500

    making cluster_df optional
    this way we can use it to preselect features as well

--- scripts/models/svc.py
+++ scripts/models/svc.py
@@ -2,7 +2,7 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_selection import SelectFromModel
 from utils.constants import NUMERIC_COLS
 
-def perform_feature_selection(stats_df, cluster_df, C=.05):
+def perform_feature_selection(stats_df, cluster_df=None, C=.05):
     X = stats_df[NUMERIC_COLS]
     y = stats_df["WINorLOSS"]
 
@@ -11,6 +11,8 @@ def perform_feature_selection(stats_df, cluster_df, C=.05):
     X_new = model.transform(X)
     X_new = X.iloc[:, model.get_support(indices=True)]
 
-    X_new = X_new[X_new.index.isin(cluster_df.index)]
-    X_new['cluster'] = cluster_df['cluster']
+    if cluster_df:
+        X_new = X_new[X_new.index.isin(cluster_df.index)]
+        X_new['cluster'] = cluster_df['cluster']
+    
     return X_new
