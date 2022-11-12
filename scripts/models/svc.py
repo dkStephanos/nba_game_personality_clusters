55aee687c98f6d58a28052ee5181961fb50370c7
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-11-12 13:00:04 -0500

    extracting scv feature selection

---
+++ scripts/models/svc.py
@@ -0,0 +1,16 @@
+from sklearn.svm import LinearSVC
+from sklearn.feature_selection import SelectFromModel
+from utils.constants import NUMERIC_COLS
+
+def perform_feature_selection(stats_df, cluster_df, C=.05):
+    X = stats_df[NUMERIC_COLS]
+    y = stats_df["WINorLOSS"]
+
+    lsvc = LinearSVC(C=0.05, penalty="l1", dual=False).fit(X, y)
+    model = SelectFromModel(lsvc, prefit=True)
+    X_new = model.transform(X)
+    X_new = X.iloc[:, model.get_support(indices=True)]
+
+    X_new = X_new[X_new.index.isin(cluster_df.index)]
+    X_new['cluster'] = cluster_df['cluster']
+    return X_new
