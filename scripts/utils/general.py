48f618ac70baa026e3879d71d10ad0a0bc79f903
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-11-12 14:57:18 -0500

    adding project cols helper

--- scripts/utils/general.py
+++ scripts/utils/general.py
@@ -1,2 +1,9 @@
 def mimic_df(df):
     return df.iloc[:0, :].copy()
+
+
+def project_cols(df, cols_a, cols_b=None):
+    if cols_b is None:
+        cols_b = df.columns
+
+    return df[[x for x in cols_a if x in cols_b]]
