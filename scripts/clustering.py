5d888dfa2a62086d52cab9c30da9ca5278d39740
Koi Stephanos <dkstephanos@gmail.com>
2023-09-23 14:03:32 -0400

    refactor

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -8,39 +8,69 @@ from models.kmeans import (
     perform_k_means,
 )
 
-_save_results = True
 
-print("Reading in data -----------\n\n")
-stats_df = pd.read_csv("./data/box_scores/nba.games.stats-clean.csv", index_col=0)
-df = pd.read_csv("./data/box_scores/nba.games.stats-normalized.csv", index_col=0)
-df.drop(df.columns[list(range(0, 6))], axis=1, inplace=True)
-print(stats_df, stats_df.columns)
+def main(
+    save_results: bool = True,
+    generate_cluster_plots: bool = True,
+    run_kmeans: bool = True,
+    calculate_distortion: bool = True,
+    get_closest_samples: bool = True
+) -> None:
+    """
+    Main function to orchestrate the execution of various analytics tasks.
 
-stats_df = perform_feature_selection(
-    stats_df.drop(stats_df.columns[list(range(0, 5))], axis=1), C=0.02
-)
-print(stats_df, stats_df.columns)
-df = project_cols(df, stats_df.columns)
+    Parameters:
+    - save_results (bool): Flag to control the saving of results to CSV.
+    - generate_cluster_plots (bool): Flag to control the generation of cluster plots.
+    - run_kmeans (bool): Flag to control the execution of the KMeans clustering model.
+    - calculate_distortion (bool): Flag to control the calculation of cluster distortion.
+    - get_closest_samples (bool): Flag to control fetching samples closest to the centroids.
+    """
 
-# print("Generating cluster plots -----------\n\n")
-# generate_elbow_plot(df, 10, save=_save_results)
-# generate_silhouette_coef_plot(df, 10, save=_save_results)
+    print("Reading in data -----------\n\n")
+    stats_df = pd.read_csv("./data/box_scores/nba.games.stats-clean.csv", index_col=0)
+    df = pd.read_csv("./data/box_scores/nba.games.stats-normalized.csv", index_col=0)
+    df.drop(df.columns[list(range(0, 6))], axis=1, inplace=True)
+    print(stats_df, stats_df.columns)
 
-print("Running the KMeans clustering model -----------\n\n")
-kmeans, y_km = perform_k_means(df)
-stats_df["cluster"] = y_km
-
-print("Calculating cluster distortion...")
-distortion_df = get_distortion_totals_per_cluster(df, kmeans, y_km)
-
-print("Getting the samples closest to the centroids...")
-closest_samples_df = get_samples_closest_to_centroid(
-    df, stats_df, kmeans, num_samples=1000
-)
-
-if _save_results:
-    stats_df.to_csv("./data/cluster_results/cluster.stats.results-raw.csv")
-    distortion_df.to_csv("./data/cluster_results/cluster.stats.results-distortion.csv")
-    closest_samples_df.to_csv(
-        "./data/cluster_results/cluster.stats.results-closest-samples.csv"
+    stats_df = perform_feature_selection(
+        stats_df.drop(stats_df.columns[list(range(0, 5))], axis=1), C=0.02
     )
+    print(stats_df, stats_df.columns)
+    df = project_cols(df, stats_df.columns)
+
+    if generate_cluster_plots:
+        print("Generating cluster plots -----------\n\n")
+        generate_elbow_plot(df, 10, save=save_results)
+        generate_silhouette_coef_plot(df, 10, save=save_results)
+
+    kmeans, y_km = None, None  # Initializing to None
+    if run_kmeans:
+        print("Running the KMeans clustering model -----------\n\n")
+        kmeans, y_km = perform_k_means(df)
+        stats_df["cluster"] = y_km
+
+    distortion_df = None  # Initializing to None
+    if calculate_distortion and kmeans is not None and y_km is not None:
+        print("Calculating cluster distortion...")
+        distortion_df = get_distortion_totals_per_cluster(df, kmeans, y_km)
+
+    closest_samples_df = None  # Initializing to None
+    if get_closest_samples and kmeans is not None:
+        print("Getting the samples closest to the centroids...")
+        closest_samples_df = get_samples_closest_to_centroid(
+            df, stats_df, kmeans, num_samples=1000
+        )
+
+    if save_results:
+        # Save DataFrames to CSV if they are not None
+        stats_df.to_csv("./data/cluster_results/cluster.stats.results-raw.csv")
+        if distortion_df is not None:
+            distortion_df.to_csv("./data/cluster_results/cluster.stats.results-distortion.csv")
+        if closest_samples_df is not None:
+            closest_samples_df.to_csv(
+                "./data/cluster_results/cluster.stats.results-closest-samples.csv"
+            )
+
+if __name__ == "__main__":
+    main(save_results=True)  # Default flags can be changed as needed
--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,22 +1,37 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-# read in raw data
-raw_games_df = pd.read_csv(
-    './data/box_scores/nba.games.stats-raw.csv', header=0, index_col=0
-)
+def normalize_and_save(save_filepath: str = './data/box_scores/nba.games.stats-normalized.csv') -> None:
+    """
+    This function reads raw data, filters for Home games, normalizes numeric data, and saves the result to a CSV file.
 
-# reduce to only Home games to avoid duplicated inputs
-raw_games_df = raw_games_df[raw_games_df.Home == "Home"]
+    Parameters:
+    - save_filepath (str): The file path where the result will be saved.
+    """
 
-# Normalize numeric data, in order to avoid feature bias in the learning module
-metadata_df = raw_games_df[raw_games_df.columns[list(range(0, 6))]]
-raw_games_df.drop(raw_games_df.columns[list(range(0, 6))], axis=1, inplace=True)
-scaled_features = StandardScaler().fit_transform(raw_games_df.values)
-scaled_features_df = pd.DataFrame(
-    scaled_features, index=raw_games_df.index, columns=raw_games_df.columns
-)
+    # Read in raw data
+    raw_games_df = pd.read_csv(
+        './data/box_scores/nba.games.stats-raw.csv', header=0, index_col=0
+    )
 
-# Adding back non-numeric data and saving to file
-result_df = metadata_df.join(scaled_features_df)
-result_df.to_csv('./data/box_scores/nba.games.stats-normalized.csv')
+    # Reduce to only Home games to avoid duplicated inputs
+    raw_games_df = raw_games_df[raw_games_df.Home == "Home"]
+
+    # Segregate metadata columns and drop them from the raw data frame
+    metadata_df = raw_games_df.iloc[:, :6]  # Assuming metadata is in the first 6 columns
+    raw_games_df = raw_games_df.drop(raw_games_df.columns[:6], axis=1)
+
+    # Normalize numeric data to avoid feature bias in the learning module
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(raw_games_df.values)
+    scaled_features_df = pd.DataFrame(
+        scaled_features, index=raw_games_df.index, columns=raw_games_df.columns
+    )
+
+    # Add back non-numeric data and save the result to a CSV file
+    result_df = metadata_df.join(scaled_features_df)
+    result_df.to_csv(save_filepath)
+
+if __name__ == "__main__":
+    # Specify the save filepath if different from the default
+    normalize_and_save()
