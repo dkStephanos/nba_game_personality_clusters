58f0b35bf698187eab550bf6c5020c008c1fd013
Koi Stephanos <dkstephanos@gmail.com>
2023-10-14 11:55:27 -0400

    refactor, pulling out utils, incorporating new procedures

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,37 +1,78 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from utils.transform import (
+    aggregate_boxscores,
+    extend_metadata,
+    extend_statistical_data,
+    normalize_df,
+)
 
-def normalize_and_save(save_filepath: str = './data/box_scores/nba.games.stats-normalized.csv') -> None:
+
+def perform_preprocessing(
+    save_filepath: str = "./data/src/nba.games.stats-normalized.csv", save_results=True
+) -> None:
     """
-    This function reads raw data, filters for Home games, normalizes numeric data, and saves the result to a CSV file.
+    This function reads raw data, aggregates it, merges it together, extends the data so each row
+    contains data for both teams, before finally filtering for Home games, normalizes numeric data,
+    and optionally saves the result to a CSV file.
 
     Parameters:
     - save_filepath (str): The file path where the result will be saved.
     """
 
     # Read in raw data
-    raw_games_df = pd.read_csv(
-        './data/box_scores/nba.games.stats-raw.csv', header=0, index_col=0
+    games_df = pd.read_csv("data/src/games.csv")
+    boxscore_df = pd.read_csv("data/src/boxscore.csv")
+    
+    # Aggregate the statiscial cols
+    aggregated_df = aggregate_boxscores(boxscore_df)
+    
+    # Merge with the games_df to get the datetime, home_team, away_team, and is_regular values
+    game_boxscore_df = pd.merge(
+        aggregated_df,
+        games_df[["game_id", "home_team", "away_team", "datetime", "is_regular"]],
+        on="game_id",
+        how="left",
     )
+
+    # Add additional cols for analysis
+    game_boxscore_df = extend_metadata(game_boxscore_df)
+    game_boxscore_df = extend_statistical_data(game_boxscore_df)
+
+    # Define the desired order of columns
+    desired_order = [
+        "game_id",
+        "team_name",
+        "datetime",
+        "is_regular",
+        "opponent",
+        "home",
+        "win",
+    ] + [
+        col
+        for col in game_boxscore_df.columns
+        if col
+        not in [
+            "game_id",
+            "team_name",
+            "datetime",
+            "is_regular",
+            "opponent",
+            "home",
+            "win",
+        ]
+    ]
+
+    # Re-order the columns in the DataFrame
+    game_boxscore_df = game_boxscore_df[desired_order]
+    game_boxscore_df = game_boxscore_df.sort_values(by=["game_id", "home", "datetime"])
 
     # Reduce to only Home games to avoid duplicated inputs
-    raw_games_df = raw_games_df[raw_games_df.Home == "Home"]
+    final_df = game_boxscore_df[game_boxscore_df.home == "Home"]
+    final_df = normalize_df(final_df)
 
-    # Segregate metadata columns and drop them from the raw data frame
-    metadata_df = raw_games_df.iloc[:, :6]  # Assuming metadata is in the first 6 columns
-    raw_games_df = raw_games_df.drop(raw_games_df.columns[:6], axis=1)
-
-    # Normalize numeric data to avoid feature bias in the learning module
-    scaler = StandardScaler()
-    scaled_features = scaler.fit_transform(raw_games_df.values)
-    scaled_features_df = pd.DataFrame(
-        scaled_features, index=raw_games_df.index, columns=raw_games_df.columns
-    )
-
-    # Add back non-numeric data and save the result to a CSV file
-    result_df = metadata_df.join(scaled_features_df)
-    result_df.to_csv(save_filepath)
+    if save_results:
+        final_df.to_csv(save_filepath, index=False)
 
 if __name__ == "__main__":
     # Specify the save filepath if different from the default
-    normalize_and_save()
+    perform_preprocessing()
