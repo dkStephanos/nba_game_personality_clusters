f439c501389c53fef89452148e52208a50293933
Koi Stephanos <dkstephanos@gmail.com>
2023-10-19 10:45:40 -0400

    extending method param support

--- scripts/models/fpgrowth.py
+++ scripts/models/fpgrowth.py
@@ -1,50 +1,49 @@
 import pandas as pd
+from typing import Optional
 from mlxtend.frequent_patterns import fpgrowth
 from mlxtend.frequent_patterns import association_rules
 
+def run_fpgrowth(
+    cluster: int,
+    df: pd.DataFrame,
+    min_support: float = 0.2,
+    min_confidence: float = 0.5,
+    max_len: Optional[int] = None,
+    verbose: bool = False,
+    save_results: bool = True,
+) -> None:
+    # Preprocess DataFrame.
+    df = df.iloc[:, 5:].astype(bool)
 
-def run_fpgrowth(min_support=0.2, min_confidence=0.5):
-    # Assuming df is your truth table DataFrame
-    # Ensure that df is a binary matrix (0 and 1 values)
-
-    df = pd.read_csv(
-        "../data/cluster_results/cluster.stats.results-truth-table.csv", index_col=0
-    )
-    df = df[df["cluster"] == 0]
-    df = df.iloc[:, 5:]
-    df = df.drop(columns=["cluster"])
-    df = df.astype(bool)
-
-    # Applying the FP-growth algorithm to find frequent itemsets
+    # Generate frequent itemsets.
     frequent_itemsets = fpgrowth(df, min_support=min_support, use_colnames=True)
 
-    # Filtering frequent itemsets to only include those containing 'win' being true
-    frequent_itemsets_win_true = frequent_itemsets[
-        frequent_itemsets["itemsets"].apply(lambda x: "win" in x)
-    ]
+    # Check if frequent itemsets contain 'win'.
+    frequent_itemsets_win = frequent_itemsets[frequent_itemsets['itemsets'].apply(lambda x: 'win' in x)]
 
-    if len(frequent_itemsets_win_true) > 0:
-        # Displaying the filtered results
-        print(frequent_itemsets_win_true)
+    if frequent_itemsets_win.empty:
+        print(f"No itemsets with 'win' for min_support={min_support}")
+        return  # Exit function if no relevant itemsets.
 
-        # Generating association rules from the filtered frequent itemsets
-        rules = association_rules(
-            frequent_itemsets_win_true,
-            metric="confidence",
-            min_threshold=min_confidence,
-            support_only=True,
-        )
+    if save_results:
+        # Save results to CSV, using the cluster number in the filename.
+        frequent_itemsets_win.to_csv(f"../data/fpgrowth_results/cluster_{cluster}_itemsets.csv", index=False)
 
-        # Further filtering rules to only include those where 'win' is true in the consequents
-        rules_win_true = rules[
-            rules["consequents"].apply(
-                lambda x: "win" in x and list(df["win"][x.index]) == [1] * len(x)
-            )
-        ]
+    # Generate association rules.
+    rules = association_rules(frequent_itemsets_win, metric='confidence', min_threshold=min_confidence)
 
-        if len(rules_win_true) > 0:
-            print(rules_win_true)
-        else:
-            print(f"No rules generated for wins with min_confidence={min_confidence}")
-    else:
-        print(f"No itemsets generated for wins with min_support={min_support}")
+    # Filter rules to get those with 'win'.
+    rules_win = rules[rules['consequents'].apply(lambda x: 'win' in x)]
+
+    if rules_win.empty:
+        print(f"No rules with 'win' for min_confidence={min_confidence}")
+        return  # Exit function if no relevant rules.
+
+    if save_results:
+        # Save rules to CSV.
+        rules_win.to_csv(f"../data/fpgrowth_results/cluster_{cluster}_rules.csv", index=False)
+
+    # Optional: Print the results (can be removed in production code).
+    if verbose:
+        print(frequent_itemsets_win)
+        print(rules_win)
