4cb35a1ad32c143f4167f94517650572412119a5
Koi Stephanos <dkstephanos@gmail.com>
2023-10-20 21:34:39 -0400

    refactor
    defaulting to .1,.3,.5,.7,.9 fro quantiles

--- scripts/utils/stats.py
+++ scripts/utils/stats.py
@@ -1,25 +1,54 @@
 import pandas as pd
 from utils.constants import NUMERIC_COLS, N_CLUSTERS
-from utils.general import mimic_df, project_cols
 
-def get_column_quantiles(stats_df, quantile=[0.2, 0.4, 0.6, 0.8], save_results=False):
-    quantiles_df = mimic_df(project_cols(stats_df, NUMERIC_COLS + ["cluster"]))
-    for cluster in range(0, N_CLUSTERS):
-        temp_df = stats_df.loc[stats_df["cluster"] == cluster][
-            [x for x in NUMERIC_COLS if x in stats_df.columns]
-        ].quantile(quantile)
-        temp_df["cluster"] = cluster
-        quantiles_df = pd.concat(
-            [
-                quantiles_df,
-                temp_df,
-            ]
-        )
 
-    if save_results:
-        quantiles_df.to_csv(
-            './data/cluster_results/cluster.stats.results-quantiles.csv'
-        )
+def get_column_quantiles(
+    stats_df,
+    quantiles=[0.1, 0.3, 0.5, 0.7, 0.9],
+    save_results=False,
+    output_path="./data/cluster_results/cluster.stats.results-quantiles.csv",
+):
+    """
+    Calculate quantiles for specified numeric columns within clusters in the dataframe.
+
+    Parameters:
+        stats_df (pd.DataFrame): The input dataframe containing the data.
+        numeric_cols (list): A list of columns for which to calculate quantiles.
+        n_clusters (int): The number of clusters in the dataframe.
+        quantiles (list): The quantiles to calculate (default is [0.2, 0.4, 0.6, 0.8]).
+        save_results (bool): Whether to save the results to a file (default is False).
+        output_path (str): The path to the file where results should be saved if save_results is True.
+
+    Returns:
+        pd.DataFrame: A dataframe containing the calculated quantiles for each cluster.
+    """
+    # Ensure the columns exist in the dataframe
+    numeric_cols = [col for col in NUMERIC_COLS if col in stats_df.columns]
+
+    # Initialize a list to hold all the temporary dataframes
+    list_of_temp_dfs = []
+
+    for cluster in range(N_CLUSTERS):
+        # Filter data for the current cluster and calculate quantiles
+        cluster_data = stats_df[stats_df["cluster"] == cluster][numeric_cols]
+        cluster_quantiles = cluster_data.quantile(quantiles)
+
+        # Turn the 'quantile' index into a column
+        cluster_quantiles = cluster_quantiles.reset_index()
+        cluster_quantiles.rename(columns={'index': 'quantile'}, inplace=True)
+
+        # Add a column for the cluster
+        cluster_quantiles['cluster'] = cluster
+
+        # Add the dataframe to the list
+        list_of_temp_dfs.append(cluster_quantiles)
+
+    # Concatenate all the individual cluster dataframes
+    quantiles_df = pd.concat(list_of_temp_dfs, ignore_index=True)
+    
+    # Save results if required
+    if save_results and output_path:
+        quantiles_df.to_csv(output_path, index=False)
 
     return quantiles_df
 
@@ -46,4 +75,3 @@ def generate_quantile_truth_table(stats_df, save_results=False):
                     result_dict[col].append(val)
 
     return pd.DataFrame.from_dict(result_dict)
-
