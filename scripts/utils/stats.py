abbce9a3bf3cfdf69d122077cf95513956318be8
Koi Stephanos <dkstephanos@gmail.com>
2023-11-18 13:12:55 -0500

    optimizing, including win col

--- scripts/utils/stats.py
+++ scripts/utils/stats.py
@@ -25,26 +25,15 @@ def get_column_quantiles(
     # Ensure the columns exist in the dataframe
     numeric_cols = [col for col in NUMERIC_COLS if col in stats_df.columns]
 
-    # Initialize a list to hold all the temporary dataframes
-    list_of_temp_dfs = []
+    # Define a function to compute quantiles for each group
+    def _compute_cluster_quantiles(group):
+        cluster_quantiles = group[numeric_cols].quantile(quantiles)
+        cluster_quantiles = cluster_quantiles.reset_index().rename(columns={'index': 'quantile'})
+        cluster_quantiles['cluster'] = group.name
+        return cluster_quantiles
 
-    for cluster in range(N_CLUSTERS):
-        # Filter data for the current cluster and calculate quantiles
-        cluster_data = stats_df[stats_df["cluster"] == cluster][numeric_cols]
-        cluster_quantiles = cluster_data.quantile(quantiles)
-
-        # Turn the 'quantile' index into a column
-        cluster_quantiles = cluster_quantiles.reset_index()
-        cluster_quantiles.rename(columns={'index': 'quantile'}, inplace=True)
-
-        # Add a column for the cluster
-        cluster_quantiles['cluster'] = cluster
-
-        # Add the dataframe to the list
-        list_of_temp_dfs.append(cluster_quantiles)
-
-    # Concatenate all the individual cluster dataframes
-    quantiles_df = pd.concat(list_of_temp_dfs, ignore_index=True)
+    # Apply the function to each cluster group
+    quantiles_df = stats_df.groupby('cluster').apply(_compute_cluster_quantiles).reset_index(drop=True)
     
     # Save results if required
     if save_results and output_path:
@@ -60,30 +49,31 @@ def generate_quantile_truth_table(stats_df, save_results=False):
     # Prepare the list of statistic columns
     statistics = [col for col in stats_df.columns if col in NUMERIC_COLS]
 
-    # Initialize the truth table with game_id and cluster information
-    truth_table_df = stats_df[['cluster']].copy()  # Assuming 'game_id' is the identifier
+    # Initialize the truth table with cluster information
+    truth_table_df = stats_df[['cluster']].copy()
+
+    # Preprocess quantiles_df for efficient lookup
+    quantile_lookup = quantiles_df.pivot(index='cluster', columns='quantile', values=statistics)
+
+   # Dictionary to hold new columns
+    new_columns = {}
 
     for percentile in QUANTILES:
         for stat in statistics:
-            # The new column for each statistic and percentile
             column_name = f"{stat}_{percentile}"
 
-            # Creating a new column with default False
-            truth_table_df[column_name] = False
+            # Vectorized comparison for the entire column
+            new_columns[column_name] = stats_df.apply(
+                lambda row: row[stat] > quantile_lookup.loc[row['cluster'], (stat, percentile)], 
+                axis=1
+            )
 
-            for index, row in stats_df.iterrows():
-                cluster = row['cluster']
-                game_stat_value = row[stat]
+    # Concatenate new columns with the original DataFrame
+    new_columns_df = pd.DataFrame(new_columns)
+    truth_table_df = pd.concat([truth_table_df, new_columns_df], axis=1)
 
-                # Get the quantile value for the current cluster and percentile
-                quantile_value = quantiles_df.loc[
-                    (quantiles_df['cluster'] == cluster) & (quantiles_df['quantile'] == percentile), 
-                    stat
-                ].values
-
-                if quantile_value.size > 0 and game_stat_value > quantile_value[0]:
-                    # If the game_stat_value is greater than the quantile value, update the truth table
-                    truth_table_df.at[index, column_name] = True
+    # Add back wins
+    truth_table_df['win'] = stats_df['win']
 
     return truth_table_df
 
