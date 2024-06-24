1fb3fe39c2fd6f323b33326b6bbbfc22fb4a9ca6
Koi Stephanos <29663585+dkStephanos@users.noreply.github.com>
2024-06-23 22:31:17 -0400

    init

---
+++ devops/00-startup.py
@@ -0,0 +1,18 @@
+# 00-startup.py
+
+print('Loading default imports...')
+
+from models.fpgrowth import run_fpgrowth
+from models.fpgrowth import mlxtend_fpgrowth
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# Verify ffmpeg path and set it for matplotlib
+ffmpeg_path = '/usr/bin/ffmpeg'
+plt.rcParams['animation.ffmpeg_path'] = ffmpeg_path
+plt.rcParams['animation.writer'] = 'ffmpeg'
+
+# Print a message to confirm the script has run
+print("Default imports loaded.")
